556048a2f25f4b1c523423a7594d180a2f484156
carr <1129579561@qqq.com>
2020-02-29 15:43:31 +0800

    App change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from "react"
 export default class App extends Component{
    render(){
      return(
-       <div>App组件</div>
+       <div>App组件第一次推送后的组件</div>
      )
    }
 }
